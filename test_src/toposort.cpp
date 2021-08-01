52212a93d6a9316b948329ceba7812f4a3aab30c
makitsukasa <allium.fistulosum.39@gmail.com>
2021-08-02 06:24:18 +0900

    3x3もソートできた

--- test_src/toposort.cpp
+++ test_src/toposort.cpp
@@ -11,6 +11,9 @@ TEST(topo_sort_tester, two_as_is) {
 	auto expected = std::vector<unsigned long>{0, 1};
 
 	ASSERT_EQ(expected.size(), actual.size()) << "Vectors are of unequal length";
+	for (unsigned long i = 0; i < expected.size(); i++) {
+		EXPECT_FLOAT_EQ(expected[i], actual[i]) << "Vectors differ at index " << i;
+	}
 }
 
 TEST(topo_sort_tester, two_reverse) {
@@ -22,4 +25,37 @@ TEST(topo_sort_tester, two_reverse) {
 	auto expected = std::vector<unsigned long>{1, 0};
 
 	ASSERT_EQ(expected.size(), actual.size()) << "Vectors are of unequal length";
+	for (unsigned long i = 0; i < expected.size(); i++) {
+		EXPECT_FLOAT_EQ(expected[i], actual[i]) << "Vectors differ at index " << i;
+	}
+}
+
+TEST(topo_sort_tester, three_as_is) {
+	std::vector<std::vector<bool>> matrix = {
+		{false, true, true},
+		{false, false, true},
+		{false, false, false},
+	};
+	auto actual   = topo_sort(matrix);
+	auto expected = std::vector<unsigned long>{0, 1, 2};
+
+	ASSERT_EQ(expected.size(), actual.size()) << "Vectors are of unequal length";
+	for (unsigned long i = 0; i < expected.size(); i++) {
+		EXPECT_FLOAT_EQ(expected[i], actual[i]) << "Vectors differ at index " << i;
+	}
+}
+
+TEST(topo_sort_tester, three_reverse) {
+	std::vector<std::vector<bool>> matrix = {
+		{false, false, false},
+		{true, false, true},
+		{true, false, false},
+	};
+	auto actual   = topo_sort(matrix);
+	auto expected = std::vector<unsigned long>{1, 2, 0};
+
+	ASSERT_EQ(expected.size(), actual.size()) << "Vectors are of unequal length";
+	for (unsigned long i = 0; i < expected.size(); i++) {
+		EXPECT_FLOAT_EQ(expected[i], actual[i]) << "Vectors differ at index " << i;
+	}
 }
