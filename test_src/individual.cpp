952738a36e24519758e05b3e12d8172623cea0f3
makitsukasa <allium.fistulosum.39@gmail.com>
2021-08-02 15:58:50 +0900

    同じテスト2回書いてた

--- test_src/individual.cpp
+++ test_src/individual.cpp
@@ -19,21 +19,6 @@ public:
 	}
 };
 
-TEST(individual_tester, calculate) {
-	enn::random_engine = std::mt19937(6700417);
-	auto ind1          = enn::individual(3, 2);
-	auto out1          = ind1.calculate(std::vector<double>({0.1, 0.2, 0.3}));
-
-	enn::random_engine = std::mt19937(6700417);
-	auto ind2          = enn::individual(3, 2);
-	auto out2          = ind2.calculate(std::vector<double>({0.1, 0.2, 0.3}));
-
-	ASSERT_EQ(out1.size(), out2.size()) << "Vectors are of unequal length";
-	for (unsigned long i = 0; i < out1.size(); i++) {
-		EXPECT_FLOAT_EQ(out1[i], out2[i]) << "Vectors differ at index " << i;
-	}
-}
-
 TEST(individual_tester, constructor_same_seed) {
 	enn::random_engine = std::mt19937(6700417);
 	auto ind1          = enn::individual(3, 2);
