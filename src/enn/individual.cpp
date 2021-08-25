72c066bf07ca559a63f30858aaa868e91141b0a8
makitsukasa <allium.fistulosum.39@gmail.com>
2021-08-26 03:47:15 +0900

    のでこうするしかない

--- src/enn/individual.cpp
+++ src/enn/individual.cpp
@@ -191,3 +191,71 @@ void enn::individual::insert(
 	// *** ソート *** ここまで
 }
 
+std::vector<double> enn::individual::calculate(std::vector<double> input) {
+	// 入力層のノードにinputを代入
+	for (unsigned long i = 0; i < num_input; i++) {
+		nodes[node_order[i]].input = input[i];
+	}
+	// バイアスノードの入力は1を代入
+	nodes[node_order[num_input]].input = 1.0;
+	// 隠れ層、出力層のノードには一旦0を代入し、下で加算していく
+	for (unsigned long i = num_input + 1; i < nodes.size(); i++) {
+		nodes[node_order[i]].input = 0.0;
+	}
+
+	// 計算
+	for (unsigned long i = 0; i < nodes.size(); i++) {
+		auto output = nodes[node_order[i]].calculate();
+		for (unsigned long j = i; j < nodes.size(); j++) {
+			if (adjacency_matrix[i][j] == nullptr) continue;
+			nodes[node_order[j]].input += adjacency_matrix[i][j]->calculate(output);
+		}
+	}
+
+	// 返り値
+	auto ret = std::vector<double>(num_output);
+	for (unsigned long i = 0; i < num_output; i++) {
+		ret[i] = nodes[node_order[nodes.size() - num_output + i]].output;
+	}
+	return ret;
+}
+
+std::vector<std::vector<bool>> enn::individual::get_bool_matrix_hidden() {
+	std::vector<std::vector<bool>> ret = {};
+
+	// num_input + 1個目(hiddenの0個目)が0になるように下駄を脱がす
+	auto jack = num_input + 1;
+
+	for (unsigned long x = 0; x < nodes.size() - jack - num_output; x++) {
+		std::vector<bool> row;
+		for (unsigned long y = 0; y < nodes.size() - jack - num_output; y++) {
+			row.push_back(adjacency_matrix[x + jack][y + jack] == nullptr ? false : true);
+		}
+		ret.push_back(row);
+	}
+	return ret;
+}
+
+namespace enn {
+std::ostream &operator<<(std::ostream &ostream, const individual &individual) {
+	ostream << "   ";
+	for (unsigned long i = 0; i < individual.nodes.size(); i++) {
+		ostream << "   " << std::setw(2) << std::setfill(' ') << individual.get_node_order()[i];
+	}
+	ostream << std::endl;
+
+	for (unsigned long i = 0; i < individual.nodes.size(); i++) {
+		ostream << std::setw(2) << std::setfill(' ') << individual.get_node_order()[i] << "  ";
+		for (unsigned long j = 0; j < individual.nodes.size(); j++) {
+			if (individual.adjacency_matrix[i][j]) {
+				ostream << std::fixed << std::setprecision(2) << *individual.adjacency_matrix[i][j]
+						<< ",";
+			} else {
+				ostream << "____,";
+			}
+		}
+		ostream << std::endl;
+	}
+	return ostream;
+}
+} // namespace enn
