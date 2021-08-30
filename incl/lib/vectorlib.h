524295642888ec31d2821b6030b3e41ecabac47f
makitsukasa <allium.fistulosum.39@gmail.com>
2021-08-30 15:51:39 +0900

    Update vectorlib.h

--- incl/lib/vectorlib.h
+++ incl/lib/vectorlib.h
@@ -2,5 +2,12 @@
 
 #include <vector>
 
-template <typename T> std::vector<T> concat(
-	std::vector<T> a, std::vector<T> b, std::vector<T> c);
+namespace enn {
+class vectorlib {
+public:
+	template <typename T> static std::vector<T> concat(
+		std::vector<T> a, std::vector<T> b, std::vector<T> c);
+
+	template <typename V> static std::vector<V> inverse(std::vector<V> &vec);
+};
+} // namespace enn
