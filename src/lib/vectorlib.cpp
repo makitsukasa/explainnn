ff001a73f53784ce443a3483b84e620d25f6e0a6
makitsukasa <allium.fistulosum.39@gmail.com>
2021-08-30 15:51:55 +0900

    Update vectorlib.cpp

--- src/lib/vectorlib.cpp
+++ src/lib/vectorlib.cpp
@@ -1,6 +1,6 @@
 #include "lib/vectorlib.h"
 
-template <typename T> std::vector<T> concat(
+template <typename T> std::vector<T> enn::vectorlib::concat(
 		std::vector<T> a, std::vector<T> b, std::vector<T> c){
 	auto ret = std::vector(a);
 	ret.insert(ret.end(), b.begin(), b.end());
@@ -8,5 +8,15 @@ template <typename T> std::vector<T> concat(
 	return ret;
 }
 
-std::vector<unsigned long> concat(
+template std::vector<unsigned long> enn::vectorlib::concat(
 		std::vector<unsigned long> a, std::vector<unsigned long> b, std::vector<unsigned long> c);
+
+template <typename V> std::vector<V> enn::vectorlib::inverse(std::vector<V> &vec) {
+	std::vector<V> inv(vec.size());
+	for (V i = 0; i < vec.size(); i++) {
+		inv[vec[i]] = i;
+	}
+	return inv;
+}
+
+template std::vector<unsigned long> enn::vectorlib::inverse(std::vector<unsigned long> &vec);
