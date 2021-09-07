b25a89204b240857f323acb924bb43c73693b9f7
makitsukasa <allium.fistulosum.39@gmail.com>
2021-09-07 13:27:38 +0900

    Update individual.h

--- incl/enn/individual.h
+++ incl/enn/individual.h
@@ -27,11 +27,15 @@ public:
 	individual(unsigned long num_input, unsigned long num_output);
 	friend std::ostream &operator<<(std::ostream &os, const individual &ind);
 	std::vector<unsigned long> get_node_order() const;
-	void add_node(node_type nodeType, unsigned long index = ULONG_MAX);
+	unsigned long add_node(node_type nodeType, unsigned long index = ULONG_MAX);
 	void add_node(unsigned long source_id, unsigned long destination_id, edge *edge1, edge *edge2);
 	void add_edge(unsigned long source_id, unsigned long destination_id, edge *edge);
-	void insert(std::vector<unsigned long> source_ids, std::vector<unsigned long> destination_ids, std::unordered_map<unsigned long, node> nodes, std::vector<std::vector<edge *>> matrix);
-	void update(std::vector<unsigned long> source_ids, std::vector<unsigned long> destination_ids, std::unordered_map<unsigned long, node> nodes, std::vector<std::vector<edge *>> matrix);
+	void insert(
+		std::vector<unsigned long> source_ids, std::vector<unsigned long> destination_ids,
+		std::unordered_map<unsigned long, node> nodes, std::vector<std::vector<edge *>> matrix);
+	void update(
+		std::vector<unsigned long> source_ids, std::vector<unsigned long> destination_ids,
+		std::unordered_map<unsigned long, node> nodes, std::vector<std::vector<edge *>> matrix);
 	std::vector<double> calculate(std::vector<double> input);
 };
 } // namespace enn
