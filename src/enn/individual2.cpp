84790a778ff74de0f80264351667ce8d69414be5
makitsukasa <allium.fistulosum.39@gmail.com>
2021-09-07 13:30:35 +0900

    Update individual2.cpp

--- src/enn/individual2.cpp
+++ src/enn/individual2.cpp
@@ -17,17 +17,22 @@ void enn::individual::insert(
 	auto jack                  = node_id_order[source_ids.back()] + 1;
 	auto additional_nodes_size = nodes.size() - source_ids.size() - destination_ids.size();
 	auto new_nodes_size        = nodes.size() + additional_nodes_size;
+	std::cout << jack << "," << additional_nodes_size << "," << new_nodes_size << std::endl;
 	// *** 一旦node_id_order[source_id]の後ろに挿入 *** ここから
+	std::vector<unsigned long> hidden_ids(additional_nodes_size);
 	for (unsigned long i = 0; i < additional_nodes_size; i++) {
-		add_node(node_type::Hidden, jack + i);
-		std::cout << jack << "+" << i << std::endl;
+		auto id       = add_node(node_type::Hidden, jack + i);
+		hidden_ids[i] = id;
 	}
 	// ノードが増えたのでnode_id_orderを再計算
 	node_id_order = vectorlib::inverse(node_order);
 	// 挿入する行列のnode_orderも計算
-	std::vector<unsigned long> hidden_ids(additional_nodes_size);
-	std::iota(hidden_ids.begin(), hidden_ids.end(), new_nodes_size - additional_nodes_size);
 	auto node_order_insertion = vectorlib::concat(source_ids, hidden_ids, destination_ids);
+
+	for (auto i : node_order_insertion) {
+		std::cout << i << ",";
+	}
+	std::cout << std::endl;
 
 	// 各rowに挿入
 	for (auto &row : adjacency_matrix) {
@@ -43,7 +48,7 @@ void enn::individual::insert(
 	for (unsigned long y = 0; y < matrix.size(); y++) {
 		auto y_ = node_id_order[node_order_insertion[y]];
 		for (unsigned long x = 0; x < matrix[y].size(); x++) {
-			auto x_ = node_id_order[node_order_insertion[x]];
+			auto x_                  = node_id_order[node_order_insertion[x]];
 			adjacency_matrix[y_][x_] = matrix[y][x];
 		}
 	}
@@ -59,6 +64,7 @@ void enn::individual::insert(
 	auto old_node_order = std::vector<unsigned long>(node_order);
 	auto jack_          = num_input + 1;
 	for (unsigned long i = 0; i < nodes.size() - jack_ - num_output; i++) {
+		// std::cout << i << "+" << jack_ << ":" << sorted_indices[i] << "+" << jack_ << std::endl;
 		node_order[i + jack_] = old_node_order[sorted_indices[i] + jack_];
 	}
 	// matrixを更新
@@ -75,72 +81,3 @@ void enn::individual::insert(
 	adjacency_matrix = new_adjacency_matrix;
 	// *** ソート *** ここまで
 }
-
-std::vector<double> enn::individual::calculate(std::vector<double> input) {
-	// 入力層のノードにinputを代入
-	for (unsigned long i = 0; i < num_input; i++) {
-		nodes[node_order[i]].input = input[i];
-	}
-	// バイアスノードの入力は1を代入
-	nodes[node_order[num_input]].input = 1.0;
-	// 隠れ層、出力層のノードには一旦0を代入し、下で加算していく
-	for (unsigned long i = num_input + 1; i < nodes.size(); i++) {
-		nodes[node_order[i]].input = 0.0;
-	}
-
-	// 計算
-	for (unsigned long i = 0; i < nodes.size(); i++) {
-		auto output = nodes[node_order[i]].calculate();
-		for (unsigned long j = i; j < nodes.size(); j++) {
-			if (adjacency_matrix[i][j] == nullptr) continue;
-			nodes[node_order[j]].input += adjacency_matrix[i][j]->calculate(output);
-		}
-	}
-
-	// 返り値
-	auto ret = std::vector<double>(num_output);
-	for (unsigned long i = 0; i < num_output; i++) {
-		ret[i] = nodes[node_order[nodes.size() - num_output + i]].output;
-	}
-	return ret;
-}
-
-std::vector<std::vector<bool>> enn::individual::get_bool_matrix_hidden() {
-	std::vector<std::vector<bool>> ret = {};
-
-	// num_input + 1個目(hiddenの0個目)が0になるように下駄を脱がす
-	auto jack = num_input + 1;
-
-	for (unsigned long x = 0; x < nodes.size() - jack - num_output; x++) {
-		std::vector<bool> row;
-		for (unsigned long y = 0; y < nodes.size() - jack - num_output; y++) {
-			row.push_back(adjacency_matrix[x + jack][y + jack] == nullptr ? false : true);
-		}
-		ret.push_back(row);
-	}
-	return ret;
-}
-
-namespace enn {
-std::ostream &operator<<(std::ostream &ostream, const individual &individual) {
-	ostream << "   ";
-	for (unsigned long i = 0; i < individual.nodes.size(); i++) {
-		ostream << "   " << std::setw(2) << std::setfill(' ') << individual.get_node_order()[i];
-	}
-	ostream << std::endl;
-
-	for (unsigned long i = 0; i < individual.nodes.size(); i++) {
-		ostream << std::setw(2) << std::setfill(' ') << individual.get_node_order()[i] << "  ";
-		for (unsigned long j = 0; j < individual.nodes.size(); j++) {
-			if (individual.adjacency_matrix[i][j]) {
-				ostream << std::fixed << std::setprecision(2) << *individual.adjacency_matrix[i][j]
-						<< ",";
-			} else {
-				ostream << "____,";
-			}
-		}
-		ostream << std::endl;
-	}
-	return ostream;
-}
-} // namespace enn
