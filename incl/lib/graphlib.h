332ea62761b0bc763214ca98cd6da2c012ea357b
makitsukasa <allium.fistulosum.39@gmail.com>
2021-09-21 17:00:01 +0900

    Update graphlib.h

--- incl/lib/graphlib.h
+++ incl/lib/graphlib.h
@@ -5,5 +5,5 @@
 std::vector<unsigned long> topo_sort(std::vector<std::vector<bool>> matrix);
 std::vector<std::vector<bool>> calc_reachablility_matrix(
 	std::vector<std::vector<bool>> adjacency_matrix, std::vector<unsigned long> source_node_ids);
-std::vector<unsigned long> get_unreachable_nodes(
+std::vector<bool> get_reachable_node_table(
 	std::vector<std::vector<bool>> adjacency_matrix, std::vector<unsigned long> source_node_ids);
